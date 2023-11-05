ded473936eae608ea4a9590e0126522aee66f6cc
MUN-HyoJin <hyojin7338@naver.com>
2023-11-05 20:57:48 +0900

    code 설명 내가 만약 코드리뷰를 했더라면

--- src/main/java/com/example/board/memberController/controller/APIController.java
+++ src/main/java/com/example/board/memberController/controller/APIController.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpSession;
 
--- src/main/java/com/example/board/memberController/dto/MemberSignUpRequestDto.java
+++ src/main/java/com/example/board/memberController/dto/MemberSignUpRequestDto.java
@@ -17,6 +17,7 @@ public class MemberSignUpRequestDto {
     @NotBlank(message = "아이디를 입력해주세요")
     private String email;
 
+    @NotBlank(message = "password 입력해주세요")
     private String password;
 
     @NotBlank(message = "닉네임을 입력해주세요.")
